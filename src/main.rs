2c8b0d59bb7b83c410778d3b3e36796000c0bc08
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2021-02-13 15:24:24 -0800

    add basic main function

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,37 @@ mod parser;
 
 use std::env;
 
+use anyhow::anyhow;
 use codegen::Codegen;
-use inkwell::context::Context;
+use inkwell::{context::Context, execution_engine::JitFunction};
 use parser::Parser;
+
+type EntryFunc = unsafe extern "C" fn() -> f64;
 
 fn main() -> anyhow::Result<()> {
     let source: String = env::args().skip(1).collect::<Vec<String>>().join(" ");
-    println!("{}", source);
+    println!("Source:");
+    println!("{}\n", source);
     let parser = Parser::default();
     let ast = parser.parse_str(&source)?;
     let context = Context::create();
+
     let mut codegen = Codegen::new(&context);
     codegen.codegen(&ast)?;
-    codegen.module.print_to_stderr();
+    println!("IR:");
+    println!("{}", codegen.module.print_to_string().to_str()?);
+
+    let ee = codegen
+        .module
+        .create_jit_execution_engine(inkwell::OptimizationLevel::None)
+        .map_err(|e| anyhow!("{}", e.to_str().unwrap()))?;
+
+    let entry: JitFunction<EntryFunc> = unsafe { ee.get_function("lambda") }?;
+
+    println!("Result:");
+    unsafe {
+        println!("{}", entry.call());
+    }
 
     Ok(())
 }
